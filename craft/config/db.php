a0df13a22c6d8a3a3a6fe6ebd5b92ab2ebcff721
jasecoop <jase@jasecoop.com>
2016-10-17 13:57:39 +0100

    Added Procfile and composer

--- craft/config/db.php
+++ craft/config/db.php
@@ -7,21 +7,13 @@
  * You can see a list of the default settings in craft/app/etc/config/defaults/db.php
  */
 
+
+$url = getenv('JAWSDB_URL');
+$dbparts = parse_url($url);
 return array(
-
-	// The database server name or IP address. Usually this is 'localhost' or '127.0.0.1'.
-	'server' => 'localhost',
-
-	// The name of the database to select.
-	'database' => 'rainmaking',
-
-	// The database username to connect with.
-	'user' => 'jase',
-
-	// The database password to connect with.
-	'password' => '',
-
-	// The prefix to use when naming tables. This can be no more than 5 characters.
-	'tablePrefix' => 'craft',
-
+  'server' => $dbparts['host'],
+  'user' => $dbparts['user'],
+  'password' => $dbparts['pass'],
+  'database' => ltrim($dbparts['path'],'/'),
+  'tablePrefix' => 'craft',
 );
