5ef9db497a2b1e01dfb729e8121c1c1505fcbf5a
jasecoop <jase@jasecoop.com>
2016-10-21 17:24:11 -0400

    home page skeleton

--- src/js/main.js
+++ src/js/main.js
@@ -3,11 +3,13 @@ var jquery = require('./lib/jquery');
 
 var craftAjax = require('./scripts/craft-ajax');
 var menu = require('./scripts/menu');
+var pillarbox = require('./scripts/pillarbox');
 
 jquery();
 
 craftAjax();
 menu();
+pillarbox();
 //
 // // Print success message to console
 // console.log('<head> scripts loaded.')
