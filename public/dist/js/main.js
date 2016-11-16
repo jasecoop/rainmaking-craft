f11d6a5510855af719a30e18627f82c47bc719e3
jasecoop <jase@jasecoop.com>
2016-11-16 11:02:36 +0000

    rain colours

--- public/dist/js/main.js
+++ public/dist/js/main.js
@@ -10864,7 +10864,7 @@ module.exports = function () {
 
   var blob = function(x, y, ran) {
     var html = '<div class="rain-content-mouse rain-content-mouse-'+ran+' fadeOut" style="top:'+y+'px; left:'+x+'px;"></div>';
-    $(html).appendTo('.rain-content').delay(2000).fadeOut(1).delay(1000).remove();
+    $(html).appendTo('.rain-content').delay(2000).fadeOut(1);
   }
 
   $(window).on('mousemove', function(e) {
