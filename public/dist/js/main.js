3ec76fe303f8ef858914556c2d2adefd7994f64c
jasecoop <jase@jasecoop.com>
2016-12-02 17:54:03 +0000

    fixes

--- public/dist/js/main.js
+++ public/dist/js/main.js
@@ -11006,15 +11006,19 @@ module.exports = function () {
     $('.border-hover a').hover(
         function(e){
           var $parent = $(this).closest('.border-hover');
+          var $title = $parent.find('.title');
           var colour = $parent.data('colour');
           var colorClass = 'border-'+colour+'-hover';
           $parent.addClass(colorClass);
+          $title.addClass('rm-' + colour);
         },
         function(e){
           var $parent = $(this).closest('.border-hover');
+          var $title = $parent.find('.title');
           var colour = $parent.data('colour');
           var colorClass = 'border-'+colour+'-hover';
           $parent.removeClass(colorClass);
+          $title.removeClass('rm-' + colour);
         }
     );
   });
